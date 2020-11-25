566eda4ab54ef851555a786207b01ad4965ffe91
Lucas Montanari <Lmonty2011@gmail.com>
2020-11-25 13:48:18 -0800

    implemented bear db and select statements

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 34, "M", "Brown", "Hungry/Agressive", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 12, "F", "Black", "Non-Agressive", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 20, "F", "White", "goofy", false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 25, "M", "Brown", "Non-Agressive", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 28, "F", "Red", "Agressive", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 56, "M", "Green", "goofy", false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 18, "F", "Black", "Agressive", false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES (NULL, 30, "M", "Black", "Very Agressive", true);
